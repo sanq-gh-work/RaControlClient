7d80e907b293233333871c4645b71e9f65ff6ab3
sanq-gh <sanq.gm.dev@gmail.com>
2014-03-21 14:48:17 +0200

    4th

--- src/sanq/ractrl/core/Const.java
+++ src/sanq/ractrl/core/Const.java
@@ -8,9 +8,6 @@ import sanq.ractrl.launcher.Main;
  * Date: 04.05.13
  * Time: 16:54
  * To change this template use File | Settings | File Templates.
- *
- * test
- *
  */
 public interface Const {
     public final static String TAG = "myApp";
